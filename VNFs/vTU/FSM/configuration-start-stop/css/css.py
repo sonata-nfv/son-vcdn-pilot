d01adcf3b3aa7d604c857c0142e13b52d78caaca
Stavros <stkolome@gmail.com>
2017-11-08 16:33:36 +0200

    Update css.py

--- VNFs/vTU/FSM/configuration-start-stop/css/css.py
+++ VNFs/vTU/FSM/configuration-start-stop/css/css.py
@@ -147,16 +147,10 @@ class CssFSM(sonSMbase):
         LOG.info("content: " + str(content.keys()))
         # TODO: Add the start logic. The content is a dictionary that contains
         # the required data
-
+        vm_image = "vtu-vnf"
         vnfr = content["vnfr"]
-        vm_image = "sonata-vtu"
-        vnfr = content["vnfr"]
-        for x in range(len(vnfr)):
-            if (content['vnfr'][x]['virtual_deployment_units']
-                    [0]['vm_image']) == vm_image:
-                mgmt_ip = (content['VNFR'][x]['virtual_deployment_units']
-                           [0]['vnfc_instance'][0]['connection_points'][0]
-                           ['type']['address'])
+        if (content['vnfd']['name']) == vm_image:
+            mgmt_ip = content['vnfr']['virtual_deployment_units'][0]['vnfc_instance'] [0]['connection_points'][0]['interface']['address']
 
         if not mgmt_ip:
             LOG.error("Couldn't obtain IP address from VNFR")
