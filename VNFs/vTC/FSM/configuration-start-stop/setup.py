eabd6cdab1d9994a1c6065f41c7668f815f9d7ac
Stavros <stkolome@gmail.com>
2017-11-09 13:56:40 +0200

    Update setup.py

--- VNFs/vTC/FSM/configuration-start-stop/setup.py
+++ VNFs/vTC/FSM/configuration-start-stop/setup.py
@@ -63,7 +63,7 @@ setup(
     keywords='NFV orchestrator',
 
     packages=find_packages("css"),
-    install_requires=['pika', 'pytest','requests','netaddr'],
+    install_requires=['pika', 'pytest','requests','netaddr','configparser'],
     setup_requires=['pytest-runner'],
 
     # To provide executable scripts, use entry points in preference to the
