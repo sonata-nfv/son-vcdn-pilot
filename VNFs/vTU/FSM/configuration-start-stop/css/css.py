2c54dd16664c355305487c457755a77a2567fecb
Thomas <thomas.soenen@ugent.be>
2017-11-09 14:09:51 +0100

    Attempt to better parse output from VNF over ssh connection.

--- VNFs/vTU/FSM/configuration-start-stop/css/css.py
+++ VNFs/vTU/FSM/configuration-start-stop/css/css.py
@@ -162,7 +162,9 @@ class CssFSM(sonSMbase):
 
 #        if sp_ip:
         ssh_client = Client(mgmt_ip, 'sonata', 'sonata', LOG, retries=10)
-        sp_ip = str(ssh_client.sendCommand('echo $SSH_CLIENT | cut -d" " -f 1'),'utf-8')
+        sp_ip_coded = ssh_client.sendCommand('echo $SSH_CLIENT | cut -d" " -f 1')
+        LOG.info("extracted coded sp_ip: " + str(sp_ip_coded))
+        sp_ip = str(sp_ip_coded, 'utf-8')
         LOG.info("extracted sp_ip: " + str(sp_ip))
         LOG.info('Mon Config: Create new conf file')
         self.createConf(sp_ip, 4, 'vtu-vnf')
